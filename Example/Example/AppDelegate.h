851de0c7e3a598151125c4312271a8c93e0f1a11
Keith Smiley <keithbsmiley@gmail.com>
2013-07-10 15:53:15 -0400

    Add example project

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Keith Smiley on 7/10/13.
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Example/Example/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Example
+//
+//  Created by Keith Smiley on 7/10/13.
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
